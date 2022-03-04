b8c0ead36d71e5ea84a63588e3ec93946c7ec7ae
Noah <NoahCKolb@gmail.com>
2022-03-03 19:58:26 -0500

    3

--- TextAdventure-master/ConsoleApp1/Program.cs
+++ TextAdventure-master/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@
 */
 
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,17 +20,28 @@ namespace ConsoleApp1
         {
             bool exit = false;
             int roomLocation = 0;
-            //Array
-            string[] rooms = new string[5] { "Entrance", "Hallway", "Atrium", "Boss room", "Treasure room" };
-            string[] weapons = new string[4] { "Sword", "Glave", "Bow", "Staff" };
-            string[] potions = new string[2] { "Healing pot", "Strength pot" };
-            string[] Treasures = new string[3] { "Gold", "Armor", "leather boot" }; 
 
-            //List
-            List<string> Mobs = new List<string>() { "Demon", "Mimic", "Baby Wyvern", "Skeleton","lich" };
-            List<string> Items = new List<string>() 
-            { "Boss door key", "Strange key", "Magic book", "Torch" };
-            //Hub
+
+
+            string[] rooms = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List rooms.txt");
+            string[] weapons = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List weapons.txt");
+            string[] potions = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List potions.txt");
+            string[] treasures = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List treasures.txt");
+            string[] mobs = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List mobs.txt");
+            string[] items = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List items.txt");
+
+            
+            /* //Array
+             string[] rooms = new string[5] { "Entrance", "Hallway", "Atrium", "Boss room", "Treasure room" };
+             string[] weapons = new string[4] { "Sword", "Glave", "Bow", "Staff" };
+             string[] potions = new string[2] { "Healing pot", "Strength pot" };
+             string[] Treasures = new string[3] { "Gold", "Armor", "leather boot" }; 
+
+             //List
+             List<string> Mobs = new List<string>() { "Demon", "Mimic", "Baby Wyvern", "Skeleton","lich" };
+             List<string> Items = new List<string>() 
+             { "Boss door key", "Strange key", "Magic book", "Torch" };
+             //Hub*/
             while (exit == false) 
             {
                 Console.WriteLine($"Your in - ({rooms[roomLocation]})");
@@ -39,36 +51,36 @@ namespace ConsoleApp1
                 Console.WriteLine("T. Display Treasures");
                 Console.WriteLine("M. Display Mobs");
                 Console.WriteLine("I. Display Items");
-                Console.WriteLine("1. Jump Right In");
+                //Console.WriteLine("1. Jump Right In");
                 Console.WriteLine("2. Exit");
                 Console.Write("Enter A Choice > ");
                 string input = Console.ReadLine();
-                /*if (input == "1")
-                {
-                    //TODO
-                }
-                else if (input == "2")
-                {
-                    exit = true;
-                }
-                else
-                {
-                    Console.WriteLine("Not an Option");
-                }*/
                 switch(input.ToLower())
                 {
-                    case "r": foreach(string value in rooms){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "w": foreach(string value in weapons){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "p": foreach(string value in potions){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "t": foreach(string value in Treasures){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "i": foreach(string value in Items){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "m": foreach(string value in Mobs){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "rooms": foreach(string value in rooms){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "weapons": foreach(string value in weapons){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "potions": foreach(string value in potions){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "treasures": foreach(string value in Treasures){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "items": foreach(string value in Items){Console.WriteLine(value); Console.WriteLine();} break;
-                    case "mobs": foreach(string value in Mobs){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "r": foreach (string line in rooms)Console.WriteLine(rooms); break;
+                        //foreach(string value in rooms){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "w": foreach (string line in weapons) Console.WriteLine(weapons); break;
+                    //foreach(string value in weapons){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "p": foreach (string line in potions) Console.WriteLine(potions); break;
+                    //foreach(string value in potions){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "t": foreach (string line in treasures) Console.WriteLine(treasures); break;
+                    //foreach(string value in Treasures){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "i": foreach (string line in items) Console.WriteLine(items); break;
+                    //foreach(string value in Items){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "m": foreach (string line in mobs) Console.WriteLine(mobs); break;
+                    //foreach(string value in Mobs){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "rooms": foreach (string line in rooms) Console.WriteLine(rooms); break;
+                        //foreach(string value in rooms){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "weapons": foreach (string line in weapons) Console.WriteLine(weapons); break;
+                    //foreach(string value in weapons){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "potions": foreach (string line in potions) Console.WriteLine(potions); break;
+                    //foreach(string value in potions){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "treasures": foreach (string line in treasures) Console.WriteLine(treasures); break;
+                    //foreach(string value in Treasures){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "items": foreach (string line in items) Console.WriteLine(items); break;
+                    //foreach(string value in Items){Console.WriteLine(value); Console.WriteLine();} break;
+                    case "mobs": foreach (string line in mobs) Console.WriteLine(mobs); break;
+                    //foreach(string value in Mobs){Console.WriteLine(value); Console.WriteLine();} break;
                     case "n": if(roomLocation != 4 ) {roomLocation++; } else {Console.WriteLine("You can't go North"); Console.WriteLine();} break;
                     case "s": if(roomLocation != 0 ) {roomLocation--;} else {Console.WriteLine("You can't go South"); Console.WriteLine();} break;
                     case "2": exit = true; break;
