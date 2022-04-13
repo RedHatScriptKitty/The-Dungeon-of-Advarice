58acebc2c0311cd6a3e630321225d97c18ad5c1b
Noah <NoahCKolb@gmail.com>
2022-04-13 11:22:02 -0400

    List file added

--- TextAdventure-master/Avarace library/playerlst.cs
+++ TextAdventure-master/Avarace library/playerlst.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Avarace_library
 {
-    class playerlst
+    public class playerlst
     {
+        public static string[] User() 
+        {
+            string userName, userClass, userRace, userID;
+            //string[] cls = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List Class ");
+            //string[] race = File.ReadAllLines(@"C:\Users\NoahC\OneDrive\Desktop\txt adventure list\List Race ");
+            using (StreamReader reader = File.OpenText(@"../../../txt adventure list\List Class"))
+                return (userID);
+        }
     }
 }
